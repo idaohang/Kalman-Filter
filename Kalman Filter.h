606afe321be31e38b595b15f7b743107e8dbb4f2
Andreas Bell Martinsen <andreas.bell@hotmail.com>
2015-12-23 16:06:33 +0100

    First comitt
    Kalman filter includes
    - Matrix library with required functions to compute the filter equations
    - Kalman filter with example system

---
+++ Kalman Filter.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Matrix.h"
+
+void compute(double y);
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Kalman Filter.h"
+#include "matrix.h"
+
+void main(){
+	compute(0.5);
+	compute(1.0);
+	compute(1.3);
+}
+
